b699fb461cce11335e4affe718212c0d51f9f416
Do0oMores <do0omores@outlook.com>
2026-03-11 13:29:14 +0800

    医疗箱

---
+++ src/main/java/top/mores/battlefield/block/MedicalSupplyBlock.java
@@ -0,0 +1,10 @@
+package top.mores.battlefield.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class MedicalSupplyBlock extends Block {
+
+    public MedicalSupplyBlock(Properties p_49795_) {
+        super(p_49795_);
+    }
+}
